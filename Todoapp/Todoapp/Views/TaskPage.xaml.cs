eb7eab90125644e8cd4af9a0a30bab3cabfb6a09
josh <j.la@eante.com.au>
2020-09-16 09:17:44 +1000

    added button functionality

--- Todoapp/Todoapp/Views/TaskPage.xaml.cs
+++ Todoapp/Todoapp/Views/TaskPage.xaml.cs
@@ -12,9 +12,23 @@ namespace Todoapp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Page1 : ContentPage
     {
+        private int numButtons = 4;
+        private string buttonText = "My Name Jeff";
+
         public Page1()
         {
             InitializeComponent();
         }
+
+        StackLayout parent;
+        public void Addbutton(object sender, EventArgs e)
+        {
+            Button newButton = new Button { Text = buttonText };
+            newButton.SetBinding(Button.CommandProperty, new Binding("ViewModelProperty"));
+            newButton.BindingContext = viewModel;
+            parent = layout;
+            parent.Children.Add(newButton);
+        }
+
     }
 }
